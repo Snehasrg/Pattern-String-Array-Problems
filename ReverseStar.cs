be21e6b65699d75786c94c5a5eea41af25cd12d5
Snehasrg <sghongade642@gmail.com>
2023-06-15 22:43:34 +0530

    [sneha]Added program to show reverse star pattern

---
+++ ReverseStar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    public class ReverseStar
+    {
+        public static void DisplayStar()
+        {
+            Console.WriteLine("Enter number for display");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = n; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
