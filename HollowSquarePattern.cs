683cc8ca338a32d49a888718c4923ad0a82be0b5
Snehasrg <sghongade642@gmail.com>
2023-06-15 22:44:19 +0530

    [sneha]Added program to show Hollow Square pattern

---
+++ HollowSquarePattern.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    public class HollowSquarePattern
+    {
+        public static void PrintPattern()
+        {
+            int number = 7;
+            for (int i = 0; i < number; i++)
+            {
+                if (i == 0 || i == 6)
+                {
+                    for (int j = 0; j < number; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+                if (i >= 1 && i <= 5)
+                {
+                    for (int j = 0; j < number; j++)
+                    {
+                        if (j == 0 || j == 6)
+                        {
+                            Console.Write("*");
+                        }
+                        else if (j >= 1 && j <= 5)
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
