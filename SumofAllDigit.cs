ea4380a11cd46656998d265e254072c199457254
Snehasrg <sghongade642@gmail.com>
2023-06-15 22:46:27 +0530

    [sneha]Added program to calculate sum of all digit

---
+++ SumofAllDigit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    public class SumofAllDigit
+    {
+        public static void CalculateSum()
+        {
+            int a = 777, sum = 0, b;
+            while (a != 0)
+            {
+                b = a % 10; //extract a digit
+                sum = sum + b; //adding the digits
+                a = a / 10; //remained number
+            }
+            Console.WriteLine("The sum of the digits is: " + sum);
+        }
+    }
+}
