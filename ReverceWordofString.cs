c99d79964cb774a2985bded911117cd816868955
Snehasrg <sghongade642@gmail.com>
2023-06-15 22:45:49 +0530

    [sneha]Added program to reverse word of string

---
+++ ReverceWordofString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    public class ReverceWordofString
+    {
+        public static void CheckReverseWord()
+        {
+            string Name = "Sneha";
+            char[] chars = Name.ToCharArray();
+            List<char> list = new List<char>();
+            Console.WriteLine("Before Reversing String");
+            foreach (char c in chars)
+            {
+                list.Add(c);
+                Console.WriteLine(c);
+            }
+            list.Reverse();
+            Console.WriteLine("After Reversing string");
+            foreach (char d in list)
+            {
+                Console.WriteLine(d);
+            }
+        }
+    }
+}
